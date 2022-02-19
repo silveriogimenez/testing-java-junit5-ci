7a051a0ab6357f04bebe75aa2c940b4e29e94400
evaldo <silverio.gimenez@gmail.com>
2022-02-19 13:41:44 +0000

    Space to trigger a build

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -13,6 +13,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
+
 class IndexControllerTest implements ControllerTests {
 
     IndexController controller;
